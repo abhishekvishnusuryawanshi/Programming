2978746e549e813936d6a842b57e2bc1184fda7d
Abhishek Suryawanshi <abhishekvishnusuryawanshi@gmail.com>
2026-06-01 21:51:17 +0530

    Add program to display digits of a number
    This program takes a user input number and displays each digit on a new line.

---
+++ C_Programming/Practice Program/program2.c
@@ -0,0 +1,32 @@
+//////////////////////////////////////////////////////////////
+//
+// This program accepts a number from the user and
+// displays its digits individually.
+//
+//////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+void DisplayDigits(int iNo)
+{
+    int iDigit = 0;
+
+    while(iNo > 0)
+    {
+        iDigit = iNo % 10;
+        printf("%d\n",iDigit);
+        iNo = iNo / 10;
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter number : \n");
+    scanf("%d",&iValue);
+
+    DisplayDigits(iValue);
+    
+    return 0;
+}
