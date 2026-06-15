61ba8e265aeaeeb54a87f62e04bd87b684abf03b
Abhishek Suryawanshi <abhishekvishnusuryawanshi@gmail.com>
2026-06-15 20:33:36 +0530

    Add program to display alternating letter pattern
    This program accepts the number of rows and columns from the user and displays a pattern of alternating uppercase and lowercase letters.

---
+++ C_Programming/Competitive Programming/program92.c
@@ -0,0 +1,44 @@
+// Accept number from user and display below pattern.
+/*
+Input:  iRow = 4    iCol = 4
+
+Output:     A   B   C   D
+            a   b   c   d
+            A   B   C   D
+            a   b   c   d      
+
+*/
+#include<stdio.h>
+
+void Pattern(int iRow , int iCol)
+{
+    int i = 0 , j = 0;
+    char ch1 = '\0',ch2 = '\0';
+
+    for(i = 1 ; i<= iRow ;i++)
+    {
+        for(j = 1 , ch2 = 'A',ch1 = 'a' ; j <= iCol ;j++,ch1++,ch2++)
+        if((i % 2)==0)
+        {
+            printf("%c \t",ch1);
+        }
+        else
+        {
+            printf("%c \t",ch2);
+        }    
+        printf("\n");
+    }
+
+}
+
+
+int main()
+{
+    int iValue1 = 0 , iValue2 = 0;
+
+    printf("Enter the number of Rows and Columns :");
+    scanf("%d %d",&iValue1,&iValue2);
+
+    Pattern(iValue1 , iValue2);
+    return 0;
+}
