0a374f9d60cf5dacdbe9e433a2b5bb310928098a
Abhishek Suryawanshi <abhishekvishnusuryawanshi@gmail.com>
2026-06-18 00:36:51 +0530

    Create program106.c

---
+++ C_Programming/Competitive Programming/program106.c
@@ -0,0 +1,51 @@
+// Accept number from user and display below pattern.
+/*
+Input:  iRow = 4   iCol = 4
+
+Output:     *   
+            *   *
+            *   *   *
+            *   *   *   *
+*/
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+
+    if(iRow != iCol)
+    {
+        printf("Enter a vaild Input ");
+        return ;
+    }
+    
+    for(i = 1; i <= iRow; i++)
+    {
+
+        for(j = 1 ; j <= iCol; j++)
+    {
+       if(i==j|| i>j )
+        {
+             printf("*\t");
+        }
+        else
+        {
+            printf(" \t");        
+        }
+
+    }
+        printf("\n");
+    }
+}
+
+
+int main()
+{
+    int iValue1 = 0 , iValue2 = 0;
+
+    printf("Enter the number of Rows and Columns :");
+    scanf("%d %d",&iValue1,&iValue2);
+
+    Pattern(iValue1 , iValue2);
+    return 0;
+}   
