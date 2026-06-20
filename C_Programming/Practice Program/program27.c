23b30bcf26543e78032fefe8d0c5692e14a4e206
Abhishek Suryawanshi <abhishekvishnusuryawanshi@gmail.com>
2026-06-20 22:46:40 +0530

    Add files via upload

---
+++ C_Programming/Practice Program/program27.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    int iValue=0;
+    int iCnt=0;
+
+    printf("Enter the Frequency : ");
+    scanf("%d",&iValue);
+
+    //Iteration
+    
+    for(iCnt = 1;iCnt<=iValue; iCnt++)
+    {
+     printf("Jay Ganesh..\n");
+    }
+    
+    return 0;
+}
