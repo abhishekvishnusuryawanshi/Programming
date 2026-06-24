1122807536450e582c9407e7bb66dfe070b21545
Abhishek Suryawanshi <abhishekvishnusuryawanshi@gmail.com>
2026-06-24 23:47:10 +0530

    Add string case toggle program in C

---
+++ C_Programming/Competitive Programming/program133.c
@@ -0,0 +1,37 @@
+/*
+Write a program which accept string from user and toggle the case.
+*/
+
+#include<stdio.h>
+
+void StrToggle(char *str)
+{
+    int i = 0;
+
+    while(str[i] != '\0')
+    {
+        if(str[i] >= 'a' && str[i] <= 'z')
+        {
+            str[i] = str[i] - 32;
+        }
+        else if (str[i] >= 'A' && str[i] <= 'Z')
+        {
+            str[i] = str[i] + 32;
+        }
+        i++;
+    }
+}
+
+int main()
+{
+    char arr[20];
+
+    printf("Enter a String : \n");
+    scanf("%[^\n]",arr);
+
+    StrToggle(arr);
+
+    printf("Updated String is : %s ",arr);
+
+    return 0;
+}
