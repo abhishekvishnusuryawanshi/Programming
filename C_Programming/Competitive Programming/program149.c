d00f126350123cb5618e9a82b5f7e08f43ef2f32
Abhishek Suryawanshi <abhishekvishnusuryawanshi@gmail.com>
2026-06-27 22:42:54 +0530

    Add program to check specific bits in a number
    This program checks whether the 7th, 8th, and 9th bits of a given number are ON or OFF.

---
+++ C_Programming/Competitive Programming/program149.c
@@ -0,0 +1,47 @@
+/*
+Write a program which which check whether 7th and 8th and 9th bit is ON or OFF.
+*/
+#include<stdio.h>
+
+typedef unsigned int UINT;
+typedef int BOOL;
+
+#define TRUE 1
+#define FALSE 0
+
+BOOL CheckBit(UINT iNo)
+{
+    UINT iMask = 0x000001C0;
+    UINT Ans = 0;
+
+    Ans = iMask & iNo ;
+
+    if(Ans == iMask )
+    {
+        return TRUE; 
+    }
+    return FALSE;
+
+}
+
+int main()
+{
+    UINT iValue = 0;
+    BOOL bRet = FALSE;
+
+    printf("Enter a Number : ");
+    scanf("%d",&iValue);
+
+    bRet = CheckBit(iValue);
+
+    if(bRet == TRUE)
+    {
+        printf("The Bit is ON");
+    }
+    else
+    {
+        printf("The Bit is OFF");
+    }
+
+    return 0;
+}
