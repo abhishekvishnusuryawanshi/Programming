1a515c54c525d8b35495e975ca1c0c71d3237fce
Abhishek Suryawanshi <abhishekvishnusuryawanshi@gmail.com>
2026-06-29 22:57:58 +0530

    Add program to off 7th bit of a number
    Implemented a program to turn off the 7th bit of a user-provided number.

---
+++ C_Programming/Competitive Programming/program156.c
@@ -0,0 +1,30 @@
+/*
+Write a program which accept one number from user and off 7th bit of that number if it is on. Return modified number.
+*/
+
+#include<stdio.h>
+typedef unsigned int UINT;
+
+UINT OffBit(UINT iNo)
+{
+    UINT iMask = 0xFFFFFFBF; ;
+    
+    return (iNo & iMask);
+
+
+}
+
+int main()
+{
+    UINT iValue = 0;
+    UINT iRet = 0;
+
+    printf("Enter a Number:");
+    scanf("%d",&iValue);
+
+    iRet = OffBit(iValue);
+
+    printf("Modified Bit is : %d",iRet);
+    
+    return 0;
+}
