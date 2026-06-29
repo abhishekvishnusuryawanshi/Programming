eba978a483a33664d08c33cee515ac3a5bb7260b
Abhishek Suryawanshi <abhishekvishnusuryawanshi@gmail.com>
2026-06-29 23:00:11 +0530

    Add program to toggle 7th bit of a number
    This program toggles the 7th bit of a given number if it is on.

---
+++ C_Programming/Competitive Programming/program158.c
@@ -0,0 +1,26 @@
+/*
+Write a program which accept one number from user and Toggle 7th bit of that number if it is on. Return modified number.
+*/
+
+#include<stdio.h>
+typedef unsigned int UINT;
+
+UINT ToggleBit(UINT iNo)
+{
+    UINT iMask = 0x00000040;
+
+    return (iNo ^ iMask) ; 
+}
+
+int main()
+{
+    UINT iValue = 0;
+    UINT iRet = 0;
+
+    printf("Enter a Number: ");
+    scanf("%d",&iValue);
+
+    iRet = ToggleBit(iValue);
+    
+    printf("The Toggle Bit is :%d",iRet);
+}
