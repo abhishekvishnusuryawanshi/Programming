66db380dabcccd751294e9ccace5025000886df1
Abhishek Suryawanshi <abhishekvishnusuryawanshi@gmail.com>
2026-06-29 19:14:43 +0530

    Add bit checking program for 5th and 18th bits
    This program checks whether the 5th and 18th bits of a given number are ON or OFF.

---
+++ C_Programming/Competitive Programming/program152.c
@@ -0,0 +1,51 @@
+/*
+Write a program which which check whether 5th and 18th bit is ON or OFF.
+*/
+
+#include<stdio.h>
+
+typedef unsigned int UINT;
+typedef int BOOL;
+
+#define TRUE 1
+#define FALSE 0
+
+BOOL ChkBit(UINT iNo)
+{
+    UINT iMask = 0x00020010;  
+    UINT Ans = 0;
+
+    Ans = iNo & iMask;
+
+    if(Ans == iMask)
+    {
+        return TRUE;
+    }
+    else
+    {
+        return FALSE;
+    }
+}
+
+
+int main()
+{
+    UINT iNo = 0;
+    BOOL bRet ;
+
+    printf("Emter a Number:");
+    scanf("%d",&iNo);
+
+    bRet = ChkBit(iNo);
+
+    if(bRet==TRUE)
+    {
+        printf("The Bit is ON");
+    }
+    else
+    {
+        printf("The Bit is OFF");
+    }
+
+    return 0;
+}
