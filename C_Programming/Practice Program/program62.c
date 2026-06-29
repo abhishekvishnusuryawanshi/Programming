e73d1390f29d07e4401ffd63b27dc8d861d496dc
Abhishek Suryawanshi <abhishekvishnusuryawanshi@gmail.com>
2026-06-29 23:27:13 +0530

    Add files via upload

---
+++ C_Programming/Practice Program/program62.c
@@ -0,0 +1,26 @@
+//INPUT : 5
+//OUTPUT : -5 -4 -3 -2 -1
+#include<stdio.h>
+
+void Display(int iNO)
+{
+    int iCnt = 0;
+    for (iCnt = -iNO; iCnt <= 0 ; iCnt++)
+    {
+        printf("%d\t",iCnt);
+    }
+
+
+}
+int main()
+{
+    int iValue=0;
+
+    printf("Enter a number:\n");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+
+
+    return 0;
+}
