1965d173a7cb3cb74a052d98775c55efea9f70ab
Abhishek Suryawanshi <abhishekvishnusuryawanshi@gmail.com>
2026-06-30 23:53:41 +0530

    Add files via upload

---
+++ Java_Programming/Practice Program/program66.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+class NumberX
+{
+    public boolean CheckPrime(int iNo)
+    {
+        int iSum = 0;
+        int iCnt = 0;
+
+        for(iCnt = 2 ;iCnt <= (iNo / 2);iCnt++)
+        {
+            if((iNo % iCnt)==0)
+            {
+               break;
+            }
+        }
+            if(iCnt < iNo/2)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+    }
+
+}
+class program66
+{
+    public static void main(String A[]) 
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iValue =0;
+        boolean bRet =false;
+        
+        System.out.println("Enter Number:");
+
+        iValue = sobj.nextInt();
+        
+        NumberX nobj = new NumberX();
+
+        bRet=nobj.CheckPrime(iValue);
+
+        if(bRet==true)
+        {
+            System.out.println("It is Prime");
+        }
+        else
+        {
+           System.out.println("It is NOT Prime"); 
+        }
+
+    }
+}
