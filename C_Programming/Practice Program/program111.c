2497d3d5bb6cf0405125598312d856064e14c6da
Abhishek Suryawanshi <abhishekvishnusuryawanshi@gmail.com>
2026-07-08 23:49:14 +0530

    Add files via upload

---
+++ C_Programming/Practice Program/program111.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+void Display(int Arr[])
+
+{
+    int icnt = 0;
+                //  Issue
+    for(icnt = 0 ; icnt <5  ; icnt++)
+    {
+        printf(" %d\n",Arr[icnt]);
+    }    
+}
+ 
+int main()
+{
+    int Brr[8] = {10,20,30,40,50,60,70,80};
+
+    Display(Brr);
+
+    return 0;
+}
