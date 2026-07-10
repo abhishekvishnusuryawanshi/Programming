954765730eff7da14ec766bf2ce8d900f7537ea2
Abhishek Suryawanshi <abhishekvishnusuryawanshi@gmail.com>
2026-07-10 22:59:39 +0530

    Add program to count even elements in linked list

---
+++ C_Programming/Competitive Programming/program172.c
@@ -0,0 +1,70 @@
+/*
+write a program to Count the Even element in singly linear linkedlist.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node *PNODE;
+typedef struct node **PPNODE;
+
+void InserFirst(PPNODE first, int iNo)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->data = iNo;
+    newn->next = NULL;
+
+    if (NULL == *first)
+    {
+        *first = newn;
+    }
+    else
+    {
+        newn->next = *first;
+        *first = newn;
+    }
+}
+
+int CountEven(PNODE first)
+{
+    int iCount = 0;
+
+    while (first != NULL)
+    {
+        if (first->data % 2 == 0)
+        {
+            iCount++;
+        }
+        first = first->next;
+    }
+
+    return iCount;
+}
+
+int main()
+{
+    PNODE head = NULL;
+    int iRet = 0;
+    int iValue = 0;
+
+    InserFirst(&head, 101);
+    InserFirst(&head, 51);
+    InserFirst(&head, 21);
+    InserFirst(&head, 11);
+
+    iRet = CountEven(head);
+
+    printf("The Even Element in the Linkedlist are : %d", iRet);
+
+    return 0;
+}
