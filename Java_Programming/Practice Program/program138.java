33c27c53863b7ff1dae3ae9a0cb4b8a2bc09d1a3
Abhishek Suryawanshi <abhishekvishnusuryawanshi@gmail.com>
2026-07-11 18:24:23 +0530

    Add files via upload

---
+++ Java_Programming/Practice Program/program138.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class program138
+{
+    public static void main(String[] args) 
+    {
+        Scanner sobj = new Scanner(System.in);
+        int iCnt = 0;
+
+        int Arr[] = {10,20,30,40,50};
+
+        for(iCnt = 0; iCnt < Arr.length; iCnt++)
+        {
+        System.out.println(Arr[iCnt]);
+        }
+        
+    }
+}
