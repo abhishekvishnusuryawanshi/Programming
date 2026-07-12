7f5912bbdb66cceb947d15e49404b925d204110f
Abhishek Suryawanshi <abhishekvishnusuryawanshi@gmail.com>
2026-07-12 22:32:33 +0530

    Add files via upload

---
+++ C++_Programming/Practice Program/program151.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+
+class ArrayX
+{
+    //Access Specifer is PRIVATE
+        int Summation(int Arr[], int iSize)
+    {
+        int iCnt = 0, iSum=0;
+
+        for(iCnt = 0; iCnt < iSize ; iCnt++)
+        {
+        iSum = iSum + Arr[iCnt];
+        }
+        return iSum;
+    }
+};
+
+int main()
+{
+    int *Brr = NULL;
+    int iLength = 0,iRet =0, iCnt = 0;
+    ArrayX aobj;
+
+    cout<<"Enter Number of Elements:\n";
+    cin>>iLength;
+
+    //C: Brr= (int *)malloc(sizeof(int) * iLength)
+    Brr = new int[iLength];
+
+    cout<<"Enter the Elements :\n";
+
+    for(iCnt = 0 ; iCnt < iLength ; iCnt++)
+    {
+        cin>>Brr[iCnt];
+    }
+
+    cout<<"Elements of Array Are:\n";
+        for(iCnt = 0 ; iCnt < iLength ; iCnt++)
+    {
+        cout<<Brr[iCnt]<<endl;
+    }
+
+    iRet = aobj.Summation(Brr,5);
+
+    cout<<"Summation :"<<iRet<<endl;
+    
+    delete[]Brr;
+   
+
+    return 0;
+}
