61cd17133c974fb8778cfeedfdac410a82472e33
Abhishek Suryawanshi <abhishekvishnusuryawanshi@gmail.com>
2026-07-13 12:03:17 +0530

    Add files via upload

---
+++ C++_Programming/Practice Program/program168.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+using namespace std;
+
+#pragma pack(1)
+
+class ArrayX
+{
+    private:
+        int *Arr;
+        int iSize;
+
+    public:    
+        //Parametrised constructor with default Argument
+        ArrayX(int X = 5)
+        {
+            iSize = X;                  
+            Arr = new int[iSize];      
+        }
+        ~ArrayX()
+        {
+            delete []Arr;           
+        }
+        void Accept()
+        {
+            int iCnt = 0;
+
+            cout<<"Enter the Elements:\n";
+            for(iCnt=0; iCnt<iSize; iCnt++)
+            {
+                cin>>Arr[iCnt];
+            }
+        }
+        void Display()
+        {
+            int iCnt = 0;
+
+            cout<<"Elements of Array are:\n";
+            for(iCnt=0; iCnt<iSize; iCnt++)
+            {
+                cout<<Arr[iCnt]<<endl;
+            }
+        }
+
+};
+
+int main()
+{
+
+    ArrayX *aobj = NULL;
+    int iLength = 0;
+
+    cout<<"Enter the number of Element:\n";
+    cin>>iLength;
+
+    aobj = new ArrayX(iLength);
+
+    aobj->Accept();
+    aobj->Display();
+
+    delete aobj;
+
+    return 0;
+}
---
+++ C++_Programming/Practice Program/program169.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+using namespace std;
+
+#pragma pack(1)
+
+class ArrayX
+{
+    private:
+        int *Arr;
+        int iSize;
+
+    public:    
+        //Parametrised constructor with default Argument
+        ArrayX(int X = 5)
+        {
+            iSize = X;                  
+            Arr = new int[iSize];      
+        }
+        ~ArrayX()
+        {
+            delete []Arr;           
+        }
+        void Accept()
+        {
+            int iCnt = 0;
+
+            cout<<"Enter the Elements:\n";
+            for(iCnt=0; iCnt<iSize; iCnt++)
+            {
+                cin>>Arr[iCnt];
+            }
+        }
+        void Display()
+        {
+            int iCnt = 0;
+
+            cout<<"Elements of Array are:\n";
+            for(iCnt=0; iCnt<iSize; iCnt++)
+            {
+                cout<<Arr[iCnt]<<endl;
+            }
+        }
+        int Summation()
+        {
+            int iCnt = 0, iSum=0;
+
+            for(iCnt = 0; iCnt < iSize ; iCnt++)
+            {
+                iSum = iSum + Arr[iCnt];
+            }
+            return iSum;
+        }
+        
+
+};
+
+int main()
+{
+
+    ArrayX *aobj = NULL;
+    int iLength = 0,iRet = 0;
+
+    cout<<"Enter the number of Element:\n";
+    cin>>iLength;
+
+    aobj = new ArrayX(iLength);
+
+    aobj->Accept();
+    aobj->Display();
+
+    iRet = aobj->Summation();
+
+    cout<<"Summation: "<<iRet;
+
+    delete aobj;
+
+    return 0;
+}
