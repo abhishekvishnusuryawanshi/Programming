2149d16964e4aef6f9891e4ed19b7ec6dd5d02b2
Abhishek Suryawanshi <abhishekvishnusuryawanshi@gmail.com>
2026-07-15 22:19:03 +0530

    Add files via upload

---
+++ Java_Programming/Practice Program/program204.java
@@ -0,0 +1,51 @@
+/* 
+    iRow = 4
+    iCol = 4
+                      Matrix
+    a   b   c   d    11  12  13  14
+    a   b   c   d    21  22  23  24
+    a   b   c   d    31  32  33  34
+    a   b   c   d    41  42  43  44
+
+*/
+import java.util.Scanner;
+
+class Pattern
+{
+  public void Display(int iRow , int iCol)
+  {
+    int i = 0 , j = 0 ;
+    char ch = '\0';
+
+    for(i  = 1 ; i<=iRow; i++)
+    {
+      for(j = 1, ch = 'a' ; j <= iCol; j++,ch++)
+      {
+        System.out.print(ch+"\t");
+      }
+      System.out.println();
+    }
+
+  }
+}
+
+class program204
+{
+  public static void main(String A[]) 
+  {
+    Scanner sobj = new Scanner(System.in);
+
+    int iValue1 = 0 ;
+    int iValue2 = 0 ;
+
+    System.out.println("Enter Number of Rows:");
+    iValue1 = sobj.nextInt();
+
+    System.out.println("Enter Number of Columns:");
+    iValue2 = sobj.nextInt();
+
+    Pattern pobj = new Pattern();
+
+    pobj.Display(iValue1,iValue2);
+  }
+}
