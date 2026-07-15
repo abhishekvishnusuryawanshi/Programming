ba75158b86c19befa44e66b7d4be23b5ffcb8152
Abhishek Suryawanshi <abhishekvishnusuryawanshi@gmail.com>
2026-07-15 22:14:34 +0530

    Add files via upload

---
+++ Java_Programming/Practice Program/program184.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class program184
+{
+    public static void Display(int iNo)
+    {
+        int iCnt = 0 ;
+        char ch = '\0';
+
+        for(iCnt = 1 , ch = 'A' ; iCnt <= iNo; iCnt++,)
+        if((iCnt % 2)==0)
+        {
+            System.out.print("*\t");
+        }
+        else
+        {
+            System.out.print(ch+"\t");
+             ch++;
+        }
+  
+    
+    }
+    public static void main(String A[] ) 
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iValue = 0;
+
+        System.out.println("Enter the Number of Elements:");
+        iValue = sobj.nextInt();
+
+        Display(iValue);
+
+
+  
+    }
+}
