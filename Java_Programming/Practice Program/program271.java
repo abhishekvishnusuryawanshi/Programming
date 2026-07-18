ae33f6a223159dc94bf5e458b78b2272dff8d77e
Abhishek Suryawanshi <abhishekvishnusuryawanshi@gmail.com>
2026-07-18 22:51:06 +0530

    Add files via upload

---
+++ Java_Programming/Practice Program/program271.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+class StringX
+{
+    public int CountCapital(String str)
+    { 
+        int i = 0;
+        int iCount = 0; 
+
+        for(i =0 ; i< str.length(); i++)
+        {
+            if(str.charAt(i) >= 'A' && str.charAt(i) <= 'Z')
+            {
+                iCount++;
+            }
+        }
+        return iCount;
+    }
+
+    public int CountSmall(String str)
+    { 
+        int i = 0;
+        int iCount = 0; 
+
+        for(i = 0 ; i< str.length(); i++)
+        {
+            if(str.charAt(i) >= 'a' && str.charAt(i) <= 'z')
+            {
+                iCount++;
+            }
+        }
+        return iCount;
+    }
+}
+
+class program271
+{
+    public static void main(String A []) 
+    {
+        StringX strobj = new StringX();
+        Scanner sobj = new Scanner(System.in);
+        String data = null;
+        int iRet = 0;
+        
+        System.out.println("Enter String:");
+        data = sobj.nextLine();
+
+        iRet = strobj.CountCapital(data);
+
+        System.out.println("Number of Capital Character: "+iRet);
+
+        iRet = strobj.CountSmall(data);
+
+        System.out.println("Number of Small Character: "+iRet);
+
+
+
+    }
+}
