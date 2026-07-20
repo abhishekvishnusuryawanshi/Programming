e8ab8a8566f6438d9c52689d356fbcc532b972b0
Abhishek Suryawanshi <abhishekvishnusuryawanshi@gmail.com>
2026-07-20 12:18:47 +0530

    Add files via upload

---
+++ Java_Programming/Practice Program/program286.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class StringX
+{
+    public String toUpperX(String str)
+    {
+        char Arr [] = str.toCharArray();        
+
+        for(int i = 0 ; i < Arr.length ; i++)
+        {
+            if(Arr[i] >= 'a' && Arr[i] <= 'z' )
+            {
+                Arr[i] = (char)(Arr[i] -32);
+            }
+        }
+         return new String(Arr);               
+
+    }
+
+}
+
+class program286
+{
+    public static void main(String A []) 
+    {
+        StringX strobj = new StringX();
+        Scanner sobj = new Scanner(System.in);
+        String data = null;
+        String sRet = null;
+        
+        System.out.println("Enter String:");
+        data = sobj.nextLine();
+
+        sRet = strobj.toUpperX(data);
+        System.out.println("Upadted String:"+sRet);
+
+
+
+
+
+    }
+}
