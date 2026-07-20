10de79e769144fb6d068409c63c0fbb45f3df4b5
Abhishek Suryawanshi <abhishekvishnusuryawanshi@gmail.com>
2026-07-20 12:19:03 +0530

    Add files via upload

---
+++ Java_Programming/Practice Program/program287.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+class StringX
+{
+    public String toUpperX(String str)
+    {
+        char Arr [] = str.toCharArray();        
+
+        for(int i = 0 ; i < Arr.length ; i++)
+        {
+            if(Arr[i] >= 'a' && Arr[i] <= 'z' )
+            {
+                Arr[i] = (char)(Arr[i] -32);
+            }
+        }
+         return new String(Arr);               
+
+    }
+
+    public String toLowerX(String str)
+    {
+        char Arr [] = str.toCharArray();        
+
+        for(int i = 0 ; i < Arr.length ; i++)
+        {
+            if(Arr[i] >= 'A' && Arr[i] <= 'Z' )
+            {
+                Arr[i] = (char)(Arr[i] +32);
+            }
+        }
+         return new String(Arr);               
+
+    }
+
+}
+
+class program287
+{
+    public static void main(String A []) 
+    {
+        StringX strobj = new StringX();
+        Scanner sobj = new Scanner(System.in);
+        String data = null;
+        String sRet = null;
+        
+        System.out.println("Enter String:");
+        data = sobj.nextLine();
+
+        sRet = strobj.toUpperX(data);
+        System.out.println("Upadted String:"+sRet);
+
+        sRet = strobj.toLowerX(data);
+        System.out.println("Upadted String:"+sRet);
+
+
+
+
+
+    }
+}
