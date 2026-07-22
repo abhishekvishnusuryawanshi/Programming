fcbf77543b811444e54e17e0d8a6edba7d5fcc2f
Abhishek Suryawanshi <abhishekvishnusuryawanshi@gmail.com>
2026-07-22 20:47:34 +0530

    Add files via upload

---
+++ C_Programming/Practice Program/program328.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+typedef unsigned int UINT;
+
+int main()
+{
+    //Smallest Value of Int
+    UINT iMask=0x00000000;
+
+    printf("%d\t%X\n",iMask,iMask);
+
+
+    return 0;
+}
---
+++ C_Programming/Practice Program/program329.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+typedef unsigned int UINT;
+
+int main()
+{
+    //Largest Value of Int
+    UINT iMask=0xFFFFFFFF;
+
+    printf("%u\t%X\n",iMask,iMask);
+
+
+    return 0;
+}
