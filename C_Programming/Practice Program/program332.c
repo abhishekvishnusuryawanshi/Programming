5d14d863a14c37d352fd9a5288ff9879c3fc997a
Abhishek Suryawanshi <abhishekvishnusuryawanshi@gmail.com>
2026-07-23 23:01:22 +0530

    Add files via upload

---
+++ C_Programming/Practice Program/program332.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+typedef unsigned int UINT;
+
+//position 28 
+int main()
+{
+    UINT iNo = 0;
+    UINT iMask = 0;
+    
+    printf("Enter a Number : \n");
+    scanf("%d",&iNo);
+
+    iMask = 0x08000000;
+
+    iNo = iNo ^ iMask ;
+
+    printf("Updated Number : %d\n",iNo);
+
+    return 0;
+}
