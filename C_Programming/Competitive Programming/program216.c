deb4df573570905179a1b983490de3498f8b9ce4
Abhishek Suryawanshi <abhishekvishnusuryawanshi@gmail.com>
2026-07-25 22:20:02 +0530

    Add files via upload

---
+++ C_Programming/Competitive Programming/program216.c
@@ -0,0 +1,29 @@
+/*
+Write a recursive program which display below pattern.
+INPUT - 4
+OUTPUT - *  *  *   * 
+*/
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    if(iNo != 0)
+    {
+        printf("*\t");
+        iNo--;
+        Display(iNo);
+    }
+
+}
+
+int main()
+{
+    int iValue = 0 ;
+
+    printf("Enter a Number : \n");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+
+    return 0;
+}
