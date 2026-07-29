c7bd1bbf8999ab9e355895d4c1dc52f31354aefa
Abhishek Suryawanshi <abhishekvishnusuryawanshi@gmail.com>
2026-07-29 22:46:57 +0530

    Add program to count lowercase characters in a file
    This program reads a file and counts the number of lowercase characters.

---
+++ C_Programming/Competitive Programming/program197.c
@@ -0,0 +1,53 @@
+/*
+    Write a program which accepts file name from user and
+    returns the number of Count Small characters in that file.
+*/
+#include<stdio.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<string.h>
+
+#define BUFFER_SIZE 1024
+
+int CountSmall(char FileName[])
+{
+    char Buffer[BUFFER_SIZE] = {'\0'};
+
+    int iRet = 0 , iCount = 0 , i = 0 ;
+    int fd = 0;
+
+    fd = open(FileName,O_RDONLY);
+
+    if(fd == -1)
+    {
+        printf("Unable to Open File\n");
+        return -1;
+    }
+
+    while((iRet = read(fd,Buffer,sizeof(Buffer)))!=0)
+    {
+        for(i = 0 ; i< iRet; i++)
+        {
+            if(Buffer[i] > 'a' || Buffer[i] < 'z')
+            {
+                iCount ++;
+            }
+        }
+        memset(Buffer,'\0',sizeof(Buffer));
+    }
+
+    return iCount;
+}
+int main()
+{
+    char Fname[30] = {'\0'};
+    int iRet = 0;
+
+    printf("Enter the File Name:\n");
+    scanf("%[^'\n']s",&Fname);
+
+    iRet = CountSmall(Fname);
+    printf("The Number of Small letter in File: %d",iRet);
+
+    return 0;
+}
