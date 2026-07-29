bd8c7d8a56749af9668c9d7371f5efa50dffeac0
Abhishek Suryawanshi <abhishekvishnusuryawanshi@gmail.com>
2026-07-29 22:49:20 +0530

    Create program to open a file in read mode
    This program prompts the user for a file name and attempts to open it in read mode. It handles success and error cases for file opening.

---
+++ C_Programming/Competitive Programming/program201.c
@@ -0,0 +1,31 @@
+/*
+    Write an application which accepts file name from user
+    and open that file in read mode.
+*/
+
+#include<stdio.h>
+#include<fcntl.h>
+#include<unistd.h>
+
+int main()
+{
+    char Fname[30];
+    int fd = 0;
+    
+    printf("Enter the File Name : ");
+    scanf("%s", Fname);
+
+    fd = open(Fname, O_RDONLY);
+
+    if(fd == -1)
+    {
+        printf("Unable to open file\n");
+    }
+    else
+    {
+        printf("File opened successfully\n");
+        close(fd);
+    }
+
+    return 0;
+}
