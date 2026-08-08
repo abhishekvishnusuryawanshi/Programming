5552746419cc5dddf4276daed1b41d34b2894597
Abhishek Suryawanshi <abhishekvishnusuryawanshi@gmail.com>
2026-08-08 18:41:42 +0530

    Create program to log directory file details
    The program accepts a directory path from the user and writes the name, size, and data of all files in that directory to a newly created file named 'Marvellous.txt'.

---
+++ Java_Programming/Competitive Programming/program40.java
@@ -0,0 +1,63 @@
+/*
+Accept a directory name from user and write name, size and data of all files from that directory into one newly created file named as "Marvellous.txt".
+*/
+
+import java.io.*;
+import java.util.Scanner;
+
+public class program255
+{
+    public static void main(String[] args) throws Exception
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.print("Enter directory path : ");
+        String dirName = sobj.nextLine();
+
+        File dir = new File(dirName);
+
+        if(dir.exists() && dir.isDirectory())
+        {
+            FileOutputStream fout = new FileOutputStream("Marvellous.txt");
+
+            File[] files = dir.listFiles();
+
+            byte[] Buffer = new byte[1024];
+            int iRet = 0;
+
+            for(File file : files)
+            {
+                if(file.isFile())
+                {
+                    String str = "----------------------------------------\n";
+                    str = str + "File Name : " + file.getName() + "\n";
+                    str = str + "File Size : " + file.length() + " Bytes\n";
+                    str = str + "----------------------------------------\n";
+
+                    fout.write(str.getBytes());
+
+                    FileInputStream fin = new FileInputStream(file);
+
+                    while((iRet = fin.read(Buffer)) != -1)
+                    {
+                        fout.write(Buffer, 0, iRet);
+                    }
+
+                    fout.write("\n\n".getBytes());
+
+                    fin.close();
+                }
+            }
+
+            fout.close();
+
+            System.out.println("File name, size and data are written into Marvellous.txt");
+        }
+        else
+        {
+            System.out.println("Invalid Directory.");
+        }
+
+        sobj.close();
+    }
+}
